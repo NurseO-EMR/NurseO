0e64316ea9c2c16cff1ec0ef1d76d06738d18b5d
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-02-06 21:32:46 -0500

    using an icon for logging out!

--- src/Components/Nav/TopMenu/TopNav.tsx
+++ src/Components/Nav/TopMenu/TopNav.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from './Logo';
 import {getAuth} from "firebase/auth"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
 type Props = React.HTMLAttributes<HTMLDivElement>
 export default class TopNav extends React.Component<Props> {
@@ -30,7 +32,10 @@ export default class TopNav extends React.Component<Props> {
                         <div className="flex items-center space-x-3">
                             <span className="font-medium rounded ">
                                 {this.auth.currentUser?.displayName ? "Hi " + this.auth.currentUser.displayName : null}&nbsp;
-                                <span className="cursor-pointer" onClick={this.onLogoutClickHandler.bind(this)}>Logout</span>
+                                <span className='font-bold'> | </span>
+                                <span className="cursor-pointer" onClick={this.onLogoutClickHandler.bind(this)}>
+                                    <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
+                                </span>
                             </span>
                         </div>
                 </div>
