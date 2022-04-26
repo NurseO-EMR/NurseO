a4017e7096c66159e3057cf9509418629a800b7f
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-04-26 11:31:45 -0400

    allowing the input box to exist without label

--- src/Components/Form/Input.tsx
+++ src/Components/Form/Input.tsx
@@ -5,16 +5,18 @@ import {LabelInputWrapper} from './LabelInputWrapper.js';
 type Props = React.HTMLProps<HTMLInputElement> & {
     id: string,
     notRequired?: boolean
-    admin?: boolean
 };
 export class Input extends React.Component<Props> {
 
     public render() {	
         return (
             <LabelInputWrapper className={this.props.className}>
-                <label htmlFor={this.props.id}>{this.props.children}</label>
-                <input onChange={this.props.onChange} className={`border-2 rounded-full ml-6 text-center col-span-3 h-9 border-${this.props.admin ? "admin" : "primary"}`}
-                 id={this.props.id} type={this.props.type} value={this.props.value} required={!this.props.notRequired}
+                {this.props.children ? <label htmlFor={this.props.id}>{this.props.children}</label> : null}
+                
+                <input onChange={this.props.onChange}
+                 className={`border-2 rounded-full text-center col-span-3 h-9 border-primary ${this.props.children ? "ml-6": null}`}
+                 id={this.props.id} type={this.props.type} value={this.props.value} 
+                 required={!this.props.notRequired}
                  list={this.props.list}
                  autoComplete={this.props.autoComplete}
                  /> 
--- src/Components/Form/LabelInputWrapper.tsx
+++ src/Components/Form/LabelInputWrapper.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 type Props = {
-    className?: string
+    className?: string,
+    hasLabel?: boolean
 }
 
 export class LabelInputWrapper extends React.Component<Props> {
 
     public render() {
         return (
-            <div className={`grid grid-cols-4 w-11/12 my-2 items-center font-bold ${this.props.className}`}>
+            <div className={`grid w-11/12 my-2 items-center font-bold 
+                             ${this.props.hasLabel ? "grid-cols-4" : null}
+                             ${this.props.className}`}>
                 {this.props.children}
             </div>
 
