a8e653ae24a42615cea6971c774d13890f74f917
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-07-17 14:26:30 -0400

    adding links to the nav

--- src/Components/nav/Nav.tsx
+++ src/Components/nav/Nav.tsx
@@ -7,17 +7,17 @@ export function Nav() {
         <nav aria-label="primary" className="absolute top-0 shadow-xl w-screen py-4 px-[10vw] h-fit flex justify-between items-center bg-white/95 z-20">
             <Link to="/"><Logo className="text-2xl" /></Link>
             <div className="flex gap-10 align-middle">
-                <Link to="">Home</Link>
+                <Link to="/">Home</Link>
                 <Dropdown label="Patients">
-                    <Link to="">Create Patient</Link>
-                    <Link to="">View/Edit Patients</Link>
+                    <Link to="/patient/create">Create Patient</Link>
+                    <Link to="/patient/view">View/Edit Patients</Link>
                 </Dropdown>
                 <Dropdown label="Medications">
-                    <Link to="">Create Medication</Link>
-                    <Link to="">View/Edit Medications</Link>
+                    <Link to="/meds/create">Create Medication</Link>
+                    <Link to="/meds/view">View/Edit Medications</Link>
                 </Dropdown>
                 <Dropdown label="Locations">
-                    <Link to="">Create Location</Link>
+                    <Link to="/locations/create">Create Location</Link>
                     <Link to="">View/Edit Locations</Link>
                 </Dropdown>
             </div>
