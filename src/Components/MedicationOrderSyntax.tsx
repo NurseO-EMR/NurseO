3f1d55a3254bdb97d4cc1cdb56d4c54806a7c3e1
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-07-21 13:06:10 -0400

    removing many of the unceccerry parts and depeciesies.

--- src/Components/MedicationOrderSyntax.tsx
+++ src/Components/MedicationOrderSyntax.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import { Medication } from '../Types/Medications.js';
 import { MedicationOrder } from '../Types/PatientProfile.js';
 
 type Props = {
     order: MedicationOrder,
     medName: string
-    
-}
-type State = {
-    medication:Medication|null
-}
-export class MedicationOrderSyntax extends React.Component<Props, State> {
 
-
-    public render() {	
-        return (
-            <>
-                    {this.props.medName} {" "}
-                    {this.props.order.concentration}{" "}
-                    {this.props.order.route}{" "}
-                    {this.props.order.frequency} {" "}
-                    {this.props.order.routine}  {" "}
-                    {this.props.order.PRNNote}{" "}
-                    {this.props.order.notes ? `(${this.props.order.notes})`: null}
-            </>
-
-        );
-    }	
 }
+export function MedicationOrderSyntax(props: Props) {
+    return (
+        <>
+            {props.medName} {" "}
+            {props.order.concentration}{" "}
+            {props.order.route}{" "}
+            {props.order.frequency} {" "}
+            {props.order.routine}  {" "}
+            {props.order.PRNNote}{" "}
+            {props.order.notes ? `(${props.order.notes})` : null}
+        </>
+
+    );
+}	
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import "./assets/react-pure-modal.min.css";
-import "./style.css"
-export * from "./Services/State.js";
-export * from "./Services/TapOutService.js";
-export * from "./Services/Util.js";
-export * from "./Components/Card/Card.js";
-export * from "./Components/Card/EmptyCard.js";
-export * from "./Components/Form/BigNavButton.js";
-export * from "./Components/Form/Button.js";
-export * from "./Components/Form/ButtonWConfirmBox.js";
-export * from "./Components/Form/Input.js";
-export * from "./Components/Form/LabelInputWrapper.js";
-export * from "./Components/Form/LoadingButton.js";
-export * from "./Components/Form/SaveButton.js";
-export * from "./Components/Form/SelectInput.js";
-export * from "./Components/Form/TextArea.js";
-export * from "./Components/Logo.js";
 export * from "./Components/MedicationOrderSyntax.js";
 export * from "./Types/ErrorCodes.js";
 export * from "./Types/Gender.js";
@@ -23,6 +6,3 @@ export * from "./Types/PatientProfile.js";
 export * from "./Types/Report.js";
 export * from "./Types/Settings.js";
 export * from "./Types/Status.js";
-export * from "./Components/Background.js"
-export * from "./Components/Form/SignInButton.js"
-export * from "./Components/Form/AnonymousSignInButton.js"
