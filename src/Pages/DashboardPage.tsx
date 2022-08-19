89e8cfedef8e71ee346ce61f6071cdffeb7c7393
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-08-19 13:49:35 -0400

    fixing an issue where the provider orders button doesn't stay at the bottom

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -16,12 +16,12 @@ export default function DashboardPage() {
 
 
     return (
-        <div>
+        <div className="overflow-x-hidden">
             <TopNav />
             <Mar orders={$patient.value.medicationOrders} simTime={$patient.value.time} />
             <div className="w-screen flex justify-center">
                 {showProviderOrdersButton ?
-                    <button onClick={onProviderOrders} className="absolute bottom-10 w-screen h-20 bg-red-700 text-xl text-white font-bold animate-pulse ">
+                    <button onClick={onProviderOrders} className=" w-screen h-20 bg-red-700 text-xl text-white font-bold animate-pulse ">
                         Click Here if you received provider orders</button> : null
                 }
 
