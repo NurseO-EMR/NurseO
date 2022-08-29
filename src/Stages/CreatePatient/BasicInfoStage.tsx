09405b8cffc11fd93b68c47695e411e623f4cbeb
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-08-29 12:47:21 -0400

    fixing an issue where dob will not show up on edit

--- src/Stages/CreatePatient/BasicInfoStage.tsx
+++ src/Stages/CreatePatient/BasicInfoStage.tsx
@@ -72,5 +72,5 @@ function convertDateToValue(date:string) {
     const d = new Date(stars);
     
 
-    return `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+    return `${d.getFullYear()}-${(d.getMonth()+1).toString().padStart(2,"0")}-${d.getDate().toString().padStart(2,"0")}`
 }
