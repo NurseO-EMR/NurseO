1ff9cc891a04df3a36b880f329c370890c88d5c8
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-09-13 14:36:04 -0400

    fixing a bug causing the logout button to crash the website

--- src/Components/Nav/TopMenu/TopNav.tsx
+++ src/Components/Nav/TopMenu/TopNav.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
 import Logo from './Logo';
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { useHistory } from 'react-router-dom';
 
 type Props = React.HTMLAttributes<HTMLDivElement>
-export default class TopNav extends React.Component<Props> {
+export default function TopNav(props: Props) {
 
-    private auth;
+    const auth = getAuth()
+    const history = useHistory();
 
-    constructor(props:Props) {
-        super(props);
-        this.auth = getAuth();
-    }
-
-    async onLogoutClickHandler() {
+    const onLogoutClickHandler = async () => {
         const auth = getAuth();
         await auth.signOut();
+        history.push("/")
         window.location.reload();
     }
 
-    public render() {
-        return (
-            <nav className={"bg-white shadow-lg " + this.props.className}>
-                    <div className="flex justify-around">
-                        <Logo />
-                        <div className="flex items-center space-x-8">
-                            {this.props.children}
-                        </div>
-
-                        <div className="flex items-center space-x-3">
-                            <span className="font-medium rounded ">
-                                {this.auth.currentUser?.displayName ? "Hi " + this.auth.currentUser.displayName : null}&nbsp;
-                                <span className='font-bold'> | </span>
-                                <span className="cursor-pointer" onClick={this.onLogoutClickHandler.bind(this)}>
-                                    <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
-                                </span>
-                            </span>
-                        </div>
+    return (
+        <nav className={"bg-white shadow-lg " + props.className}>
+            <div className="flex justify-around">
+                <Logo />
+                <div className="flex items-center space-x-8">
+                    {props.children}
                 </div>
-            </nav>
 
-        );
-    }
+                <div className="flex items-center space-x-3">
+                    <span className="font-medium rounded ">
+                        {auth.currentUser?.displayName ? "Hi " + auth.currentUser.displayName : null}&nbsp;
+                        <span className='font-bold'> | </span>
+                        <span className="cursor-pointer" onClick={onLogoutClickHandler}>
+                            <FontAwesomeIcon icon={faSignOutAlt}></FontAwesomeIcon>
+                        </span>
+                    </span>
+                </div>
+            </div>
+        </nav>
+
+    );
 }
