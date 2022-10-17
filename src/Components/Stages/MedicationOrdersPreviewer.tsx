2fdb899cb9b2575b074da3580ad138dd2668e159
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-10-17 14:20:16 -0400

    updating to NurseO core v 0.15.0

--- src/Components/Stages/MedicationOrdersPreviewer.tsx
+++ src/Components/Stages/MedicationOrdersPreviewer.tsx
@@ -1,7 +1,7 @@
 import { faSquareCaretDown, faSquareCaretUp, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion} from "framer-motion";
-import { MedicationOrder, MedicationOrderSyntax } from "nurse-o-core";
+import { Medication, MedicationOrder, MedicationOrderSyntax } from "nurse-o-core";
 import { useEffect, useState } from "react";
 import { Database } from "../../Services/Database";
 type Props = {
@@ -13,21 +13,26 @@ type Props = {
 
 export function MedicationOrdersPreviewer(props: Props) {
 
-    const [medName, setMedName] = useState("Loading...")
+    const [med, setMed] = useState<Medication>({
+        id: "", locations: [], narcoticCountNeeded: false
+    })
 
     useEffect(()=>{
         const db = Database.getInstance();
         db.getMedication(props.order.id).then(m=>{
-            if(m) setMedName(m.brandName || m.genericName || "")
-            else setMedName("Error")
+            if(m) setMed(med)
+            else {
+                med.genericName="Error"
+                setMed(med)
+            }
         })
-    },[props.order.id])
+    },[med, props.order.id])
 
     return (
         <motion.div className="bg-gray shadow-xl w-formWidth rounded-lg overflow-y-hidden py-5 mb-8 flex justify-evenly "
              initial={{scaleY:0 }} animate={{scaleY:1 }}>
             <div>
-                <div className="text-center font-bold text-blue w-72"><MedicationOrderSyntax medName={medName} order={props.order} /></div>
+                <div className="text-center font-bold text-blue w-72"><MedicationOrderSyntax med={med} order={props.order} /></div>
                 <div>Type: {props.order.orderType}</div>
                 <div className="mt-3">Mar: {props.order.mar.length > 0 ? props.order.mar.map((time) => time.hour.toString().padStart(2,"0") + ":" + time.minutes.toString().padStart(2,"0")) : "No mar data added"}
                 </div>
