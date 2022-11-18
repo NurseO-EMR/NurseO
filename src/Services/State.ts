3b994505662843213bce8f4ad4d6e6b5ec959473
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-11-18 10:47:35 -0500

    removing the commented code

--- src/Services/State.ts
+++ src/Services/State.ts
@@ -2,6 +2,5 @@ import { PatientChart } from "nurse-o-core";
 import { BehaviorSubject } from "rxjs";
 
 export const $patient = new BehaviorSubject<PatientChart>(new PatientChart());
-// export const $providerOrdersAvailable = new BehaviorSubject<boolean>(false);
 export const $locationID = new BehaviorSubject<string | null>(null);
 export const $showVerify = new BehaviorSubject<boolean>(true);
