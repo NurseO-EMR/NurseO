3ed311519f7fb35ca7047ffde68a467ab04e11f3
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2022-12-06 11:35:47 -0500

    adding the onChange Logic for the dynamic table

--- src/Components/Reports/Viewer/ReportDynamicTable.tsx
+++ src/Components/Reports/Viewer/ReportDynamicTable.tsx
@@ -1,5 +1,6 @@
 import { Report, StudentReport } from "nurse-o-core";
 import { useEffect, useState } from "react";
+import { Button } from "../../Form/Button";
 import { Input } from "../../Form/Input";
 import { SearchableSelect } from "../../Form/SearchableSelect";
 import { Td } from "../../Table/Td";
@@ -8,16 +9,16 @@ import { Tr } from "../../Table/Tr";
 type Props = {
     setName: string,
     options: Report[],
-    onChange: (report:StudentReport) => void
+    onChange: (report:StudentReport[]) => void
 }
 
 export function ReportDynamicTable(props: Props) {
-    const sampleTable = [
-        ["", "hello2", "hello3"],
-        ["", "2", "3"],
-        ["","5","6"]
+    const InitialTable = [
+        ["", "", ""],
+        ["", "", ""],
+        ["","",""]
     ]
-    const [table, setTable] = useState(sampleTable)
+    const [table, setTable] = useState<string[][]>(InitialTable)
     const [options, setOptions] = useState<{name:string}[]>(props.options.map(o=>{return {name:o.name}}))
 
     useEffect(()=>{
@@ -52,7 +53,7 @@ export function ReportDynamicTable(props: Props) {
         // if last row has a key add an empty row
         if(table[table.length-1][0].length > 0) table.push(new Array<string>(table[0].length).fill("",0,table[0].length))
         
-        
+
         // if the last column is filled add another column
         if(table[0][table[0].length-1].length > 0) {
             for(let rowIndex = 0; rowIndex<table.length; rowIndex++) {
@@ -63,15 +64,44 @@ export function ReportDynamicTable(props: Props) {
         setTable([...table])
     }
 
+
+    const onSaveClickHandler = ()=>{
+        const times = table[0]
+        const reports:StudentReport[] = []
+
+        // -1 to remove the last row as that is the new one
+        // start from 1 as 0 is the times
+        for(let row = 1; row<table.length-1; row++) {
+            const key = table[row][0]
+
+            // -1 to remove the last column as that is the last one
+            //start from one as 0 is the keys 
+            for(let column = 1; column<table[row].length-1; column++) {
+
+                const report:StudentReport = {
+                    date: times[column],
+                    reportType: "studentVitalsReport",
+                    setName: props.setName,
+                    time: "",
+                    value: table[row][column],
+                    vitalName: key
+                }
+                reports.push(report)
+            }
+            
+        }
+
+        props.onChange(reports)
+        
+    }
+
     return <div>
         <table className="w-full relative">
-            <thead>
-            </thead>
             <tbody>
                 {table.map((row,rowIndex)=><Tr key={rowIndex}>
                     {row.map((cell,columnIndex)=>{
                         // top left corner should have nothing 
-                        if(rowIndex+columnIndex === 0) return <Td key={columnIndex}><Input hideLabel label={"Empty"} disabled/></Td>
+                        if(rowIndex+columnIndex === 0) return <Td key={columnIndex}><Input hideLabel label={"Empty"} value={`Time/${props.setName}`} readOnly disabled/></Td>
                         //keys
                         else if (columnIndex === 0) return <Td key={columnIndex}><SearchableSelect hideLabel
                                                                  label={""} options={options} value={cell}
@@ -81,7 +111,7 @@ export function ReportDynamicTable(props: Props) {
                                                                  /></Td>
 
                         // time
-                        if(rowIndex === 0) return <Td key={columnIndex}><Input hideLabel label={"time"}
+                        if(rowIndex === 0) return <Td key={columnIndex}><Input hideLabel label="Day/Time" placeholder="Day/Time"
                          value={cell} onChange={e=>onTimeupdateHandler(columnIndex, e.currentTarget.value)}/></Td>
 
                         // values
@@ -92,5 +122,6 @@ export function ReportDynamicTable(props: Props) {
                 </Tr>)}
             </tbody>
         </table>
+        <Button className="w-full bg-blue mt-5" onClick={onSaveClickHandler}>Save</Button>
     </div>
 }
