0d56dd43cba417457a044ca629cb79693b1ed961
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2023-01-15 14:54:49 -0500

    adding the ui for switching suffixes

--- src/Components/Form/Input.tsx
+++ src/Components/Form/Input.tsx
@@ -1,18 +1,22 @@
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion, Variants } from "framer-motion";
-import { DetailedHTMLProps, ForwardedRef, forwardRef, InputHTMLAttributes, useState } from "react";
+import { ChangeEvent, DetailedHTMLProps, ForwardedRef, forwardRef, InputHTMLAttributes, useEffect, useState } from "react";
 import {v4 as uuid} from "uuid"
+import { Announcement, broadcastAnnouncement } from "../../Services/AnnouncementService";
 
 export type Props = Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "id"> & {
     optional?: boolean,
     label: string,
     delay?: number,
-    suffix?: string,
+    suffix?: string[],
     hideLabel?: boolean,
-    inputClassName?: string
+    inputClassName?: string,
 }
 
 function InputEle(props: Props, ref:ForwardedRef<HTMLInputElement>) {
     const [id] = useState(uuid())
+    const [suffixIndex, setSuffixIndex] = useState(0)
     const animationVariants:Variants = { 
         hidden: { opacity: 0 },
         show: { 
@@ -23,10 +27,41 @@ function InputEle(props: Props, ref:ForwardedRef<HTMLInputElement>) {
     }
 
 
+    // useEffect(()=>{
+    //     if(props.suffix && props.suffix.length > 0 && props.value) {
+    //         const value = props.value.toString()
+    //         let numberValue = "";
+    //         let suffixValue = "";
+    //         for(const char of value) {
+    //             if(char === "." || !isNaN(parseInt(char))) numberValue += char
+    //             else suffixValue+="";
+    //         }
+
+    //         const index = props.suffix.indexOf(suffixValue)
+    //         if(index > -1) setSuffixIndex(index)
+    //         setValue(numberValue)
+
+    //     }
+    // }, [props.suffix, props.value])
+
     const getInputProps = () =>{
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { optional, label, delay, required, className, hideLabel, ...inputProps} = props
         return inputProps
+    }
+
+    const onSuffixSwitchHandler = ()=>{
+        if(!props.suffix) {
+            broadcastAnnouncement("There is no suffix to switch to", Announcement.error)
+            return;
+        }
+
+        let index = suffixIndex
+        if(suffixIndex === props.suffix.length - 1) index = 0
+        else index++;
+
+        setSuffixIndex(index)
+
     }
 
     return (
@@ -37,10 +72,15 @@ function InputEle(props: Props, ref:ForwardedRef<HTMLInputElement>) {
                 <span className="opacity-75 text-sm"> {props.optional ? "(optional)" : null}</span>
             </label>
             <input id={id} {...getInputProps()} 
-            required={!props.optional} 
-            className={`border h-8  px-2 w-full ${props.inputClassName}`} ref={ref} />
+                required={!props.optional} 
+                className={`border h-8  px-2 w-full ${props.inputClassName}`} ref={ref} 
 
-            {props.suffix ? <div className="absolute top-7 left-78/100 tracking-wider opacity-75">| {props.suffix}</div>: null}
+            />
+
+            {props.suffix ? <div className="absolute top-7 left-78/100 tracking-wider opacity-75"> | 
+                {props.suffix.length > 1 ? <> {props.suffix[suffixIndex]} <FontAwesomeIcon className="cursor-pointer" onClick={onSuffixSwitchHandler}  icon={faArrowsRotate} /></> 
+                    : props.suffix[suffixIndex] }
+            </div>: null}
         </motion.div>
     )
 }
