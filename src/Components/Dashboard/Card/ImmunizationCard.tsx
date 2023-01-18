29a60e4c9c6f28773eb0c2edb02c6647283161ee
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2023-01-17 22:07:19 -0500

    fixing key issue

--- src/Components/Dashboard/Card/ImmunizationCard.tsx
+++ src/Components/Dashboard/Card/ImmunizationCard.tsx
@@ -6,26 +6,24 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
     preview?: boolean
 }
 
-export default class ImmunizationCard extends React.Component<Props> {
+export default function ImmunizationCard(props:Props) {
 
-    public render() {
         return (
-            <Card className={this.props.className} title="Immunizations" admin={this.props.preview}>
+            <Card className={props.className} title="Immunizations" admin={props.preview}>
             <thead className="font-bold">
                 <tr>
                     <td className="border-2 p-2">Taken Immunization</td>
                 </tr>
             </thead>
             <tbody>
-                {this.props.immunizations?.length === 0 ? 
+                {props.immunizations?.length === 0 ? 
                     <tr><td colSpan={2} className='text-center p-2'><h1>No immunizations record found</h1></td></tr>:
-                    this.props.immunizations!.map((immunization,i) => <tr className='border'>
-                        <td className='px-4 py-2' key={i}>{immunization}</td>
-                    </tr> )
+                    props.immunizations!.map((immunization,i) => <tr className='border'  key={i}>
+                        <td className='px-4 py-2'>{immunization}</td>
+                    </tr> ) 
                 }
             </tbody>
         </Card>
 
         );
     }	
-}
