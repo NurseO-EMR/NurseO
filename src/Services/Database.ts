38dd5c59258816f5f260ecf0a130d7baf709c703
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2023-02-10 13:32:07 -0500

    removing emulator

--- src/Services/Database.ts
+++ src/Services/Database.ts
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import {
     addDoc, collection, DocumentReference, getDocs, getFirestore,
     limit, query, updateDoc, where, doc, getDoc, orderBy,
-    connectFirestoreEmulator
+    // connectFirestoreEmulator
 } from "firebase/firestore";
 import { $error, $locationID, $patient, $settings } from "./State";
 import firebaseConfig from "./../firebaseConfig.json";
@@ -23,7 +23,7 @@ export default class Database {
     constructor() {
         initializeApp(firebaseConfig);
         this.db = getFirestore();
-        connectFirestoreEmulator(this.db, 'localhost', 8080);
+        // connectFirestoreEmulator(this.db, 'localhost', 8080);
         this.patientDocRef = null;
         this.currentPatientID = null;
         this.cache = new Cache();
