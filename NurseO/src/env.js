0e27c9f953b7c666ebf263b7eb74362839b3bfab
Mohammed Alkhafaji <moahmmed199898@users.noreply.github.com>
2025-01-05 20:26:25 -0500

    simplifying deployment

--- NurseO/src/env.js
+++ NurseO/src/env.js
@@ -35,7 +35,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_basePath: z.string().optional(),
+    NEXT_PUBLIC_basePath: z.string(),
     NEXT_PUBLIC_domain: z.string()
   },
 
