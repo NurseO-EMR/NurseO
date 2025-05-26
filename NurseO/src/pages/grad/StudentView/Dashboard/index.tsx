c8caf50e84167d7f82a8e7f22bf52f9a88a00bbc
Mohammed Alkhafaji <moahmmed199898@users.noreply.github.com>
2025-05-26 09:57:25 -0400

    fixing the style of the dashboard

--- NurseO/src/pages/grad/StudentView/Dashboard/index.tsx
+++ NurseO/src/pages/grad/StudentView/Dashboard/index.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { OrderKind } from '~/core/index';
 import AllergyCard from '~/components/EMR/Dashboard/Card/AllergyCard';
-import FlagsCard from '~/components/EMR/Dashboard/Card/FlagsCard';
 import HistoryCard from '~/components/EMR/Dashboard/Card/HistoryCard';
 import ImmunizationCard from '~/components/EMR/Dashboard/Card/ImmunizationCard';
 import SocialHistoryCard from '~/components/EMR/Dashboard/Card/SocialHistory';
@@ -21,8 +20,7 @@ export default function Index() {
             <div className="grid grid-cols-4 mx-4 grid-in-main">
                 <DiagnosisCard className="col-span-2" diagnosis={patient.diagnosis} />
                 <ChiefComplaintCard className='col-span-2' chiefComplaint={patient.chiefComplaint} />
-                <FlagsCard className="col-span-1" flags={patient?.flags}></FlagsCard>
-                <ImmunizationCard className="col-span-2" immunizations={patient.immunizations} />
+                <ImmunizationCard className="col-span-3" immunizations={patient.immunizations} />
                 <SocialHistoryCard className="col-span-1" history={patient.socialHistory} />
                 <HistoryCard className="col-span-3" history={patient.medicalHistory} />
                 <AllergyCard className='col-span-1' allergies={patient?.allergies}></AllergyCard>
