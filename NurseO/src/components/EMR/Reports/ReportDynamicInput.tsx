d54f2c1b6c3b183d588744ffa47a87cc293c7ba2
Mohammed Alkhafaji <moahmmed199898@users.noreply.github.com>
2025-07-18 20:40:57 -0400

    handling the check boxes check

--- NurseO/src/components/EMR/Reports/ReportDynamicInput.tsx
+++ NurseO/src/components/EMR/Reports/ReportDynamicInput.tsx
@@ -3,18 +3,32 @@ import { type ReportSet, type ReportField } from "~/core"
 import { Input } from "~/components/common/ui/input"
 import { Select } from "~/components/common/ui/select";
 import { Checkbox } from "~/components/common/ui/checkbox";
+import { useState } from "react";
 
 type Props = {
     set: ReportSet
     field: ReportField
     disabled: boolean,
     onChange: (value: string) => void,
-    value?: string | undefined
 }
+
 export function ReportDynamicInput(props: Props) {
     const { set, field } = props;
     const id = `${set.name}-${field.name}`
     const className = "grid grid-cols-4 py-3 items-center even:bg-gray-200 w-full pl-10"
+    const [checkedBoxes, setCheckedBoxes] = useState<string[]>([])
+
+    const onCheckboxCheck = (label: string, checked: boolean) => {
+        if (checked) {
+            checkedBoxes.push(label)
+        } else {
+            const index = checkedBoxes.indexOf(label)
+            checkedBoxes.splice(index, 1)
+        }
+
+        setCheckedBoxes([...checkedBoxes])
+        props.onChange(checkedBoxes.join(", "))
+    }
 
     if (props.field.fieldType === "text") {
         return (
@@ -43,7 +57,7 @@ export function ReportDynamicInput(props: Props) {
                 <div className="flex items-start w-full flex-wrap gap-x-10 gap-y-10 col-span-3 pr-48">
                     {field.options?.map(o => (
                         <div key={o} className="flex items-center space-x-2">
-                            <Checkbox id={`${id}-${o}`} disabled={props.disabled} />
+                            <Checkbox id={`${id}-${o}`} disabled={props.disabled} onCheckedChange={e => onCheckboxCheck(o, e === true)} />
                             <Label htmlFor={`${id}-${o}`} className="text-sm">{o}</Label>
                         </div>
                     ))}
